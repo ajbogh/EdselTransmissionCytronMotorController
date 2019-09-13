172cbc2849179b7968b792232027d8115ed4a5a8
Allan Bogh <ajbogh@allanbogh.com>
2019-09-12 22:40:06 -0700

    Update with v3 code

--- EdselTransmissionCytronMotorController.ino
+++ EdselTransmissionCytronMotorController.ino
@@ -2,18 +2,17 @@
  * PIN CONFIGURATION                                                           *
  *******************************************************************************/
 
-//const int servoPin = 4
-const int parkPin = 8;
-const int reversePin = 9;
-const int neutralPin = 10;
-const int drivePin = 11;
-const int lowPin = 12;
+const int parkPin = 6;
+const int reversePin = 7;
+const int neutralPin = 8;
+const int drivePin = 9;
+const int lowPin = 10;
 
-// PWM is connected to pin 3.
-const int pinPwm = 3;
+// PWM is connected to pin 11.
+const int pinPwm = 11;
 
-// DIR is connected to pin a digital pin 2.
-const int pinDir = 2;
+// DIR is connected to pin a digital pin 13.
+const int pinDir = 13;
 
 // Analog pins
 const int potPos = 5;
@@ -23,13 +22,13 @@ const int potPos = 5;
  *******************************************************************************/
 
 // precision is about 3 units
-int limits[2] = {60,980};	// set limitations {min,max}
+int limits[2] = {40,870};	// set limitations {min,max}
 
-int parkPos = 60; // 60-80 = 20
-int reversePos = 165; // 100-230 = 130 // ~130 == 1/2"
-int neutralPos = 310; //240-380 = 140
-int drivePos = 465; // 590 is max for drive
-int lowPos = 750;  //980 is max
+int parkPos = 680; // 680
+int reversePos = 540;
+int neutralPos = 400; //240-380 = 140
+int drivePos = 260; // 590 is max for drive
+int lowPos = 100;  //980 is max
 
 int parkState = 0; // when 1 then the park button is pressed
 int reverseState = 0;
@@ -38,6 +37,10 @@ int driveState = 0;
 int lowState = 0;
 
 int dir = 1;
+int inward = -1;
+int outward = 1;
+
+int leeway = 3;
 
 boolean targetHit = false;
 int lastStatePos = 0;
@@ -61,36 +64,41 @@ void setup() {
 void moveMotor(int targetPos, int currentPos, boolean stopMotion) {
   if(stopMotion){
     dir = 0;
-  } else if(currentPos == targetPos || currentPos == targetPos + 1 || currentPos == targetPos - 1) {
+  } else if(
+    currentPos == targetPos || 
+    currentPos == targetPos + leeway || 
+    currentPos == targetPos - leeway) {
     Serial.print(" - targetHit - ");
     dir = 0;
     targetHit = true;
   } else if(currentPos < targetPos || currentPos < limits[0]){
-    dir = -1; 
+    dir = outward; 
   } else if(currentPos > targetPos || currentPos > limits[1]){
-    dir = 1;
+    dir = inward;
   }
 
   Serial.print(" - ");  
   Serial.print(dir);
+  Serial.print(" - ");
+  Serial.print(currentPos);
+  Serial.print(':');
+  Serial.print(targetPos);
   
   if(dir == 0){
     // no motion
     analogWrite(pinPwm, 0);
     digitalWrite(pinDir, LOW);
-  } else if(dir == 1) {
+  } else if(dir == outward) {
     // outward
     Serial.print(" - outward");
     analogWrite(pinPwm, 255); 
     digitalWrite(pinDir, LOW);
-  } else if(dir == -1) {
+  } else if(dir == inward) {
     // inward
     Serial.print(" - inward");
     analogWrite(pinPwm, 255);
     digitalWrite(pinDir, HIGH);
   }
-  
-  Serial.println();
 }
 
 void checkBeforeMove(int positionTarget, int potPosition, boolean stopMotion) {
@@ -100,12 +108,18 @@ void checkBeforeMove(int positionTarget, int potPosition, boolean stopMotion) {
   lastStatePos = positionTarget;
   if(!targetHit){
     moveMotor(positionTarget, potPosition, false);
+  } else {
+    Serial.print(" - no move - ");
+    Serial.print(potPosition);
+    Serial.print(":");
+    Serial.print(positionTarget);
   }
 }
 
 void loop() 
 { 
   int potPosition =  analogRead(potPos);
+  Serial.print(potPosition);
   parkState = digitalRead(parkPin);
   reverseState = digitalRead(reversePin);
   neutralState = digitalRead(neutralPin);
@@ -113,24 +127,26 @@ void loop()
   lowState = digitalRead(lowPin);
   
   if(parkState == 1){
-    Serial.print("parkState");
+    Serial.print("P");
     checkBeforeMove(parkPos, potPosition, false);
   } else if(reverseState == 1){
-    Serial.print("reverseState");
+    Serial.print("R");
     checkBeforeMove(reversePos, potPosition, false);
   } else if(neutralState == 1){
-    Serial.print("neutralState");
+    Serial.print("N");
     checkBeforeMove(neutralPos, potPosition, false);
   } else if(driveState == 1){
-    Serial.print("driveState");
+    Serial.print("D");
     checkBeforeMove(drivePos, potPosition, false);
   } else if(lowState == 1){
-    Serial.print("lowState");
+    Serial.print("L");
     checkBeforeMove(lowPos, potPosition, false);
   } else {
     // stop motor
     moveMotor(limits[0], potPosition, true);
   }
+  
+  Serial.println();
   
   delay(20);
 } 
